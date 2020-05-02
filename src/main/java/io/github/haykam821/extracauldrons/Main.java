d0cb6f4d912fd9d444a801a900cba9cfb5590ced
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-05-02 05:48:10 -0400

    Add soaked brick cauldrons

--- src/main/java/io/github/haykam821/extracauldrons/Main.java
+++ src/main/java/io/github/haykam821/extracauldrons/Main.java
@@ -234,6 +234,7 @@ public class Main implements ModInitializer {
 			registerCauldron("diorite_brick", new Identifier("blockus", "diorite_bricks"));
 			registerCauldron("granite_brick", new Identifier("blockus", "granite_bricks"));
 			registerCauldron("magma_brick", new Identifier("blockus", "magma_bricks"));
+			registerCauldron("soaked_brick", new Identifier("blockus", "soaked_bricks"));
 			registerCauldron("honeycomb_brick", new Identifier("blockus", "honeycomb_bricks"));
 			registerLavaCauldron("obsidian_brick", new Identifier("blockus", "obsidian_bricks"));
 			registerLavaCauldron("blaze_brick", new Identifier("blockus", "blaze_bricks"));
