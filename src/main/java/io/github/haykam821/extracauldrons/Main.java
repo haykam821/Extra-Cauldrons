3c482cbeb085ffae0d47eb551309a7759edac5a4
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-05-02 06:15:27 -0400

    Add bluestone tile cauldrons

--- src/main/java/io/github/haykam821/extracauldrons/Main.java
+++ src/main/java/io/github/haykam821/extracauldrons/Main.java
@@ -251,6 +251,7 @@ public class Main implements ModInitializer {
 			registerCauldron("marble_brick", new Identifier("blockus", "marble_bricks"));
 			registerCauldron("bluestone", new Identifier("blockus", "bluestone"));
 			registerCauldron("bluestone_brick", new Identifier("blockus", "bluestone_bricks"));
+			registerCauldron("bluestone_tile", new Identifier("blockus", "bluestone_tiles"));
 
 			// Stained stone brick
 			registerCauldron("white_stone_brick", new Identifier("blockus", "white_stone_bricks"));
